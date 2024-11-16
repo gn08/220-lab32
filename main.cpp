c8e22f1fe6ee361f2dcd3591e4b802555745f666
gn08 <gabbyfilipova@gmail.com>
2024-11-15 19:04:23 -0800

    commit 7:00

--- main.cpp
+++ main.cpp
@@ -6,29 +6,33 @@
 
 using namespace std;
 const int INITIAL_SIZE = 2;
-
+//does_foo() set the toll que with amount of cars
+//return integer
 int main() {
+    //random time
     srand(time(0));
+    //set variable
     int time_step = 1;
 
     deque<Car> toll_queue;
     for (int i=0; i< INITIAL_SIZE; ++i){
         toll_queue.push_back(Car());
     }
-
+//current que
     cout << "Initial queue: ";
     for (const Car& car: toll_queue){
         car.print();
     }
     cout << endl;
-
+//toll booth for each time step
     while (!toll_queue.empty()){
         int operation = rand() % 100 + 1;
-
+// if less than or equal to 55 leave que
         if (operation <= 55){
             cout << "Time" << time_step << "Operation: Car paid: ";
             toll_queue.front().print();
             toll_queue.pop_front();
+        // new car joins
         } else {
             Car new_car;
             cout << "Time" << time_step << "Operation: Joined lane: ";
@@ -37,7 +41,7 @@ int main() {
         }
 
         cout << "Queue: " << endl;
-
+//print current state
         if(toll_queue.empty()){
             cout << "   Empty" << endl;
         }else{
@@ -46,7 +50,7 @@ int main() {
             }
         }
         cout << endl;
-
+//increment time
         ++time_step;
     }
     return 0;
