16b25bdddd4dca92f835e065ffd3a5e59b7ed33b
gn08 <gabbyfilipova@gmail.com>
2024-11-17 21:15:13 -0800

    commit 9:15

--- main2.cpp
+++ main2.cpp
@@ -3,6 +3,7 @@
 #include "Car.h"
 #include <deque>
 #include <ctime>
+#include <string>
 
 using namespace std;
 
@@ -12,10 +13,6 @@ const double PROB_PAY = 0.46;
 const double PROB_JOIN = 0.39;
 const double PROB_SWITCH = 0.15;
 const int SIMULATION_TIMES = 20;
-deque<Car> lanes[NUM_LANES];
-
-//does_foo() set the toll que with amount of cars
-//return integer
 
 class Car{
 private:
@@ -33,16 +30,16 @@ public:
 
 };
 
-void initialize_lanes(deque<Car< lanes[], int num_lanes){
+void initialize_lanes(deque<Car> lanes[], int num_lanes){
     for (int i = 0; i < num_lanes; i ++){
-        int num_cars = ran() % MAX_INITIAL + 1;
+        int num_cars = rand() % MAX_INITIAL + 1;
         for (int j = 0; j < num_cars; j++){
             lanes[i].push_back(Car("CarMake" + to_string(rand() % 100), 1990 + rand() % 34, rand() % 10000));
         }
     }
 }
 
-void switch_lane(){
+void switch_lane(deque<Car> lanes[], int current_lane){
     if (lanes[current_lane].size() > 1){
         int new_lane;
         do{
@@ -60,10 +57,8 @@ int main() {
     srand(time(0));
     deque<Car> lanes[NUM_LANES];
     initialize_lanes(lanes, NUM_LANES);
-
-    deque<Car> toll_queue;
 //current que
-    cout << "Initial queue: ";
+    cout << "Initial queue: " << endl;
 
     for (int lane = 0; lane < NUM_LANES; lane++){
         cout << "Lane " << lane + 1 << " Queue: ";
@@ -79,7 +74,7 @@ int main() {
 
     for (int time =1; time <= SIMULATION_TIMES; time ++){
         cout << "Time: " << time << endl;
-        for (int lane = 0; lance < NUM_LANES; lanes++){
+        for (int lane = 0; lane < NUM_LANES; lane++){
             if(!lanes[lane].empty()){
                 double rand_val= (double)rand() / RAND_MAX;
 
@@ -88,9 +83,7 @@ int main() {
                     lanes[lane].pop_front();
 
                     cout << "Lane " << lane + 1 << " Paid: [" << car << "]" << endl;
-                }
-
-                else if(rand_val < PROB_PAY + PROB_JOIN){
+                } else if(rand_val < PROB_PAY + PROB_JOIN){
                     Car new_car("CarMake" + to_string(rand() % 100), 1990 + rand() % 34, rand() % 10000);
                     lanes[lane].push_back (new_car);
                     cout << "Lane" << lane + 1 << " Joined: [" << new_car << "]" << endl;
@@ -101,6 +94,17 @@ int main() {
         }
     }
 
-    for (int lane = 0; lane< NUM)
+    for (int lane = 0; lane< NUM_LANES; lane++){
+        cout << "Lane " << lane + 1 << "Queue: ";
+        if (lanes[lane].empty()){
+            cout << "empty";
+        } else {
+            for (const Car& car: lanes[lane]){
+                cout << "[" << car << "]";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
 
 }
